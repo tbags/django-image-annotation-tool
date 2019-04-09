8d41ca607052fe262b4af711e34fdc9f6516724b
Prateeth Nayak <nayak044@umn.edu>
2019-04-08 22:34:00 -0400

    refactored css part

---
+++ annotator/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/prateethvnayak/Documents/Vat-Project/django-image-annotation-tool/annotator/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
