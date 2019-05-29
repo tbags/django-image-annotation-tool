e8fbc1752cd1d05d049340ad017d2907cb3a205b
sanjayyr <sanjayyriit@gmail.com>
2019-05-29 18:18:32 -0500

    Migrating to MySQL

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ source myenv/bin/activate
 ```shell
 pip install -r requirements.txt
 ```
+4. Configuring MySQL and migrating from SQLite3 to MySQL
+```shell
+https://www.shubhamdipt.com/blog/django-transfer-data-from-sqlite-to-another-database/
+https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-18-04
+https://www.digitalocean.com/community/tutorials/how-to-use-mysql-or-mariadb-with-your-django-application-on-ubuntu-14-04
+```
 
 ## Runserver
 ```shell
--- viapp/settings.py
+++ viapp/settings.py
@@ -85,8 +85,14 @@ WSGI_APPLICATION = 'viapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'image_annotator',
+        'USER': 'imageannotator',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
