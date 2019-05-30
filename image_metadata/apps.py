9782ca7f093feae4abac8f84238c9decede3424f
Shashank Hegde <shashankhegde@gmail.com>
2019-05-30 02:41:00 -0500

    SLayer Code

--- image_metadata/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageMetadataConfig(AppConfig):
-    name = 'image_metadata'
--- image_metadata/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-import jsonfield
-
-class ImageMetadata(models.Model):
-    """ImageMetadata"""
-
-    content = jsonfield.JSONField()
