4e2af5ff0d48a2a85cd4a380be4877932648542f
Shashank Hegde <shashankhegde@gmail.com>
2019-06-01 13:34:00 -0700

    tweak thy docker file to meet MacOS restrictions

--- mysql_docker/run_container.sh
+++ mysql_docker/run_container.sh
@@ -3,7 +3,7 @@
 sudo docker run \
     -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=django_rot_db \
     -v "$(pwd)/db/mysql":/var/lib/mysql-files \
-    vats_sql:t1
+    vats_sql:latest
     #--mount src="$(pwd)/db",target=/project/rotq_db,type=bind \
     #mysql
     #hegi:scratch #/bin/bash
